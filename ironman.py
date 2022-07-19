70a4ba1f697f5dcfa313edb181339d28c651fed9
PUNEET KUMAR <83579380+Puneet1421@users.noreply.github.com>
2022-07-19 14:00:36 +0530

    Add files via upload

---
+++ ironman.py
@@ -0,0 +1,54 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+
+
+
+engine = pyttsx3.init('sapi5')
+voices= engine.getProperty('voices') 
+print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio) 
+    engine.runAndWait() 
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print(f"User said: {query}\n")  #User query will be printed.
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")   #Say that again will be printed in case of improper voice 
+        return "None" #None string will be returned
+    return query
+
+ 
+    
+if __name__=="__main__" :
+    wishMe()
+    takeCommand()
+ 
